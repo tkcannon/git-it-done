dc95f8fa8736cf743d951a51dfa4acc17bf29a49
tkcannon <taylorkcannon@gmail.com>
2021-11-24 11:57:55 -0700

    added warning for excess of 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,7 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 function getReopIssues(repo) {
-    console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
     fetch(apiUrl).then(function (response) {
@@ -9,6 +9,11 @@ function getReopIssues(repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -55,6 +60,19 @@ function displayIssues(issues) {
         // append to the dom
         issueContainerEl.appendChild(issueEl);
     }
+}
+
+function displayWarning(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 }
 
 getReopIssues("facebook/react");
